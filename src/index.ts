52556c2f0a55f2727c1f13211798477d693f6894
kwaitsing <131563118+kwaitsing@users.noreply.github.com>
2025-02-11 13:13:13 +0800

    Init commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,41 @@
 import { Elysia } from "elysia";
+import { Logestic } from "logestic";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .onError(({ error, set }) => {
+    if (set.status === 500) {
+      return `${error}`
+    }
+    return {
+      //@ts-ignore
+      status: error.status,
+      msg: `${error}`
+    }
+  })
+  .mapResponse(({ response, set, path }) => {
+    if (path.includes('/swagger')) return;
+    return new Response(
+      JSON.stringify({
+        status: set.status,
+        ...(set.status !== 500 ? { data: response } : { msg: response })
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8'
+        }
+      }
+    )
+  })
+  .guard({
+    // Your vaildation here
+  }, (app) =>
+    app
+      .use(Logestic.preset('fancy'))
+      // Load your controllers below
+  )
+  // Routes that no vaildation process is required
+  ;
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `App is running at ${app.server?.hostname}:${app.server?.port}`
 );
